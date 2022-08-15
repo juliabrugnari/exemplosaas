63c15ed8f48856b4c0fd72d9ef393fcb0199192e
Julia Brugnari Pedro Longo <juliabrugnari@gmail.com>
2022-08-15 23:39:56 +0000

    gravando a idade

--- exemplo.java
+++ exemplo.java
@@ -3,5 +3,8 @@ public class exemplo{
     public static void main(String[] args) {
         
             System.out.println("Pedro");
+            int idade=18;
+            System.out.println("tenho"+idade);
     }
 }
+
